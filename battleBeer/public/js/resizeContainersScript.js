c6ccc474d8861cc28ce0c6999619510bb9749ccf
Gijs Groote <g.s.groote@student.tudelft.nl>
2018-12-02 11:32:49 +0100

    I got no idea what the change is

---
+++ battleBeer/public/js/resizeContainersScript.js
@@ -0,0 +1,34 @@
+
+// script makes sure the main container is in the middle
+
+// when the page is done loading, resize the containers
+$(document).ready(resizeContainers)
+
+// when the viewport is changed, resize the containers
+$(window).resize(resizeContainers)
+
+// function to set the width of the left and right container
+// only resizes containers if there is space
+function resizeContainers() {
+    
+    // viewport width and width of the middle container
+    var screenWidth = window.innerWidth;
+    var containerMiddleWidth = document.getElementById("containerMiddle").offsetWidth
+    
+    // check if the middle container is not larger than the viewport
+    if (containerMiddleWidth >= screenWidth) {
+        document.getElementById('containerLeft').setAttribute("style", "width:0px");
+        document.getElementById('containerRight').setAttribute("style", "width:0px");
+    }
+    else {
+        // calculate the space which can be added left and right containers
+        var sidesWidth = (screenWidth - containerMiddleWidth) / 2;
+        
+        // set the width of the left and right container
+        document.getElementById('containerLeft').setAttribute("style", "width:" + sidesWidth + "px");
+        document.getElementById('containerRight').setAttribute("style", "width:" + sidesWidth + "px");
+    }
+}
+
+
+
