108616f42e5a88a5627b55a2358cddf730140984
Gijs Groote <g.s.groote@student.tudelft.nl>
2018-12-03 12:10:29 +0100

    created State Transition diagram,  started on Iframe redirecting

---
+++ battleBeer/public/js/enterNameScript.js
@@ -0,0 +1,15 @@
+// document.getElementById("enterNameForm").onsubmit = function(){
+    
+
+//     console.log("is the fucntion called?")
+//     // check if the form input is correct
+
+//     // not empty go to state 2
+
+//     // one of the 2 player names field empty? go to namecheck page state 1
+   
+
+
+//     changeState("placeBeerP1");
+
+// }
--- battleBeer/public/js/gameScript.js
+++ battleBeer/public/js/gameScript.js
@@ -1,1 +1,33 @@
-// TODO
+document.getElementById("button").onclick = function(){
+    
+    
+    console.log("is the fucntion called?")
+    // check if the form input is correct
+
+    // not empty go to state 2
+
+    // one of the 2 player names field empty? go to namecheck page state 1
+   
+
+
+    changeState("placeBeerP1");
+
+}
+
+
+
+function changeState(state) {
+
+    // check if this state does actually exist
+
+    // i think there should be more checks 'vo security
+
+    console.log("changestate called + state " + state)
+
+  //  $("#iFrame").attr("src", "../iFrameScreen/" + state + "Screen.html");
+
+
+  document.getElementById("iFrame").src = "http://www.weer.nl/";
+
+
+}
