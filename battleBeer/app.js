e406eddf80cdf05d882df215069b8debbb2cde61
Gijs Groote <g.s.groote@student.tudelft.nl>
2018-12-10 09:56:22 +0100

    created server side functionality

--- battleBeer/app.js
+++ battleBeer/app.js
@@ -1,9 +1,55 @@
 var express = require("express");
 var http = require("http");
+var path = require('path');
 
-var port = process.argv[2];
+// choose a port name 
+var port = process.argv[2] || 3000;
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+
+// set the public folder as static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the view directory
+app.set("views", __dirname + "/public");
+
+// use embedded javaScript as view engine
+app.set("view engine", "ejs");
+
+
+/**
+ * routes to sent to incoming get and post requests
+ */
+var routesHTML = require("./modules_group_44/routes")(app);
+
+
+// create a server listening on port 
 http.createServer(app).listen(port);
-// copied this in from the assignment
+console.log("Server listening on port " + port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
